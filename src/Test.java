0e1569eb1a4b1809249aff95f93602efcdecf1c3
dairo <dairo@10.108.17.212>
2020-02-10 15:17:46 +0100

    Added Group 4

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		List<Group> groups = new ArrayList<>();
+		Group groep4 = new Group(4);
+		groep4.addMember(new Student("Willem Juwet", "KU Leuven"));
+		groep4.addMember(new Student("Sam Dobbelaere", "KU Leuven"));
+		groep4.addMember(new Student("Dairo de Ruck", "KU Leuven"));
 		for(Group group : groups) {
 			group.sayHi();
 		}
