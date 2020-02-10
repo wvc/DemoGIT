c9e6d5022f849f2a1eb45e02f036b750a5a7c6d9
romeo <romeo.permentier@gmail.com>
2020-02-10 15:17:07 +0100

    added group1 added Romeo Permentier to group1

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,11 @@ public class Test {
 	public static void main(String[] args) {
 		
 		List<Group> groups = new ArrayList<>();
+		
+		
+		Group group1 = new Group(1);
+		group1.addMember(new Student("Romeo Permentier", "KUL"));
+		
 		for(Group group : groups) {
 			group.sayHi();
 		}
