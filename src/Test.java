1d91c5a0aa9a2d7ca9651afe1fff92a020f4c332
wvc <wim.vancroonenburg@gmail.com>
2020-02-10 14:28:13 +0100

    added list to test

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		Person person = new Employee("Wim Vancroonenburg","KU Leuven");
-		person.sayHi();
+		List<Person> persons = new ArrayList<>();
+		
+		persons.add(new Employee("Wim Vancroonenburg", "KU Leuven"));
+		
+		
+		for(Person person : persons) {
+			person.sayHi();
+		}
 	}
 
 }
