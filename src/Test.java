ec9fde1d06b7818e448bf0561adef4d5f12abe96
jonas <jonas@10.108.17.210>
2020-02-10 15:16:24 +0100

    Added group 5

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		List<Group> groups = new ArrayList<>();
+		
+		Group groep5 = new Group(5);
+		groep5.addMember(new Student("Jonas Welvaert","KU Leuven"));
+		
 		for(Group group : groups) {
 			group.sayHi();
 		}
