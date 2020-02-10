8fe7143a18feac2d395a178e2052fda5e843bd98
wille <willem.juwet@gmail.com>
2020-02-10 15:26:00 +0100

    Willem Juwet group 4

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,8 @@ public class Test {
 		Group groep4 = new Group(4);
 		groep4.addMember(new Student("Dairo de Ruck", "KU Leuven"));
 		groep4.addMember(new Student("Sam Dobbelaere", "KU Leuven"));
+		groep4.addMember(new Student("Willem Juwet", "KU Leuven"));
+
 		for(Group group : groups) {
 			group.sayHi();
 		}
