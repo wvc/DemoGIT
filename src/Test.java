999217b043593535d3fcd1d941042275848af0a1
wvc <wim.vancroonenburg@gmail.com>
2020-02-10 15:16:26 +0100

    New branch - grouping experiment

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		List<Group> groups = new ArrayList<>();
+		
+		Group groupAdmin = new Group(0);
+		groupAdmin.addMember(new Employee("Wim Vancroonenburg","KU Leuven"));
+		
 		for(Group group : groups) {
 			group.sayHi();
 		}
