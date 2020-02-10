e58ce21fbf31722d3b8b89e0fbca4983d40f0bd2
piete <piete@ASUS-PIETER>
2020-02-10 15:17:23 +0100

    add group 2

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,9 @@ public class Test {
 	public static void main(String[] args) {
 		
 		List<Group> groups = new ArrayList<>();
+		
+		Group group2 = new Group(2);
+		group2.addMember(new Student("Pieter Carlu", "KU Leuven"));
 		for(Group group : groups) {
 			group.sayHi();
 		}
